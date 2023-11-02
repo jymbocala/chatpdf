3d59df87c2ad24bae531a9f54778183949e14aab
jymbocala <jym.bocala@gmail.com>
2023-11-03 00:07:26 +1100

    add initial homepage elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
 import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth(); // Returns string if authenticated otherwise null
+  const isAuth = !!userId; // Double negations turns truthy and falsey values into a true boolen. This enusres that isAuth is a boolean value
   return (
-    <Button>Click me</Button>
-  )
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-200 via-red-200 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="m-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+
+          <p className="max-w-xl mt-1 text-lg decoration-slate-600	">
+            Join millions of students, researchers and professionals to
+            instantly answer questions and understand research with AI.
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>fileupload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>Login to get Started!
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
