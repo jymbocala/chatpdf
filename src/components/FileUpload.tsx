806309e7116dda727642f01c4a9e84ae29bc78b9
jymbocala <jym.bocala@gmail.com>
2023-11-03 14:40:27 +1100

    install react-dropzone

---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,14 @@
+'use client'
+// Client Components allows you to write interactive UI that can be rendered on the client at request time.
+// Docs: https://nextjs.org/docs/app/building-your-application/rendering/client-components
+import React from 'react'
+
+type Props = {}
+
+const FileUpload = (props: Props) => {
+  return (
+    <div>FileUpload</div>
+  )
+}
+
+export default FileUpload
