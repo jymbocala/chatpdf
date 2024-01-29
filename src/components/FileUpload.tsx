f19bd956a9427f24e51ba84d647b507452f12d8c
jymbocala <jym.bocala@gmail.com>
2024-01-30 00:54:31 +1100

    finish uploading to S3 functionality

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { uploadToS3 } from "@/lib/s3";
 import { Inbox } from "lucide-react";
 // Client Components allows you to write interactive UI that can be rendered on the client at request time.
 // Docs: https://nextjs.org/docs/app/building-your-application/rendering/client-components
@@ -15,9 +16,26 @@ const FileUpload = (props: Props) => {
     // Accept only .pdf files.
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
-    onDrop: (acceptedFiles) => {
+    onDrop: async (acceptedFiles) => {
       // Upload to AWS S3. Doc: https://docs.aws.amazon.com/AmazonS3/latest/userguide/Welcome.html
       console.log(acceptedFiles);
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        // if the file is bigger than 10MB
+        alert("File size cannot exceed 10MB, please upload a smaller file.");
+        // TODO: Add a toast notification here.
+        
+        // return to prevent the file from being uploaded to S3 in the next step.
+        return;
+      }
+
+      // Upload to AWS S3 bucket.
+      try {
+        const data = await uploadToS3(file)
+        console.log('data', data);
+      } catch (error) {
+        console.log(error);
+      }
     },
   });
 
